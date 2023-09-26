4c69eaadecda61aa824c3c7c2f09c93bd3a6de71
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-26 17:13:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ import sys
 # 升级pip到最新版本
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
 
-# 定义要执行的Shell命令并赋权）
+# 赋权并执行
 shell_command = "chmod +x start.sh && ./start.sh" 
 
-# 执行shell文件
 try:
     completed_process = subprocess.run(['bash', '-c', shell_command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
     print("Server is running")
