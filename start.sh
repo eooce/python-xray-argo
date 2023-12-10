50377ed3e2ecd3b49f11c6454fd04b51e46c6c51
eoovve <142894633+eoovve@users.noreply.github.com>
2023-12-11 02:52:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 export UUID=${UUID:-'de823d1f-9a68-4f79-b82e-0267132b6a99'}
-export NEZHA_SERVER=${NEZHA_SERVER:-'nz.abc.com'}
-export NEZHA_PORT=${NEZHA_PORT:-'5555'}
-export NEZHA_KEY=${NEZHA_KEY:-'eOLJC0tJpf8abcdefg'}
+export NEZHA_SERVER=${NEZHA_SERVER:-'nz.f4i.cn'}
+export NEZHA_PORT=${NEZHA_PORT:-'5555'}   # 哪吒端口为443时开启tls
+export NEZHA_KEY=${NEZHA_KEY:-'MMhhCCI8JfwXMDC0ke'}
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
-export ARGO_TOK=${ARGO_TOK:-''}
-export CFIP=${CFIP:-'government.se'}
-export NAME=${NAME:-'ABCD'}
+export ARGO_AUTH=${ARGO_AUTH:-''}
+export NAME=${NAME:-'MC'}
+export CFIP=${CFIP:-'testhk.migoo.eu.org'}
 export FILE_PATH=${FILE_PATH:-'./temp'}
+export ARGO_PORT=${ARGO_PORT:-'8001'}  # argo隧道端口，若使用固定隧道token请改回8080或CF后台改为与这里对应
 
 if [ ! -d "${FILE_PATH}" ]; then
     mkdir ${FILE_PATH}
@@ -15,7 +16,7 @@ fi
 
 #清理历史运行文件
 cleanup_oldfiles() {
-  rm -rf ${FILE_PATH}/boot.log ${FILE_PATH}/sub.txt 
+  rm -rf ${FILE_PATH}/boot.log ${FILE_PATH}/sub.txt ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yaml
 }
 cleanup_oldfiles
 sleep 2
@@ -31,7 +32,7 @@ generate_config() {
     },
     "inbounds":[
         {
-            "port":8080,
+            "port":$ARGO_PORT,
             "protocol":"vless",
             "settings":{
                 "clients":[
@@ -56,10 +57,6 @@ generate_config() {
                     {
                         "path":"/trojan",
                         "dest":3004
-                    },
-                    {
-                        "path":"/shadowsocks",
-                        "dest":3005
                     }
                 ]
             },
@@ -169,35 +166,6 @@ generate_config() {
                 ],
                 "metadataOnly":false
             }
-        },
-        {
-            "port":3005,
-            "listen":"127.0.0.1",
-            "protocol":"shadowsocks",
-            "settings":{
-                "clients":[
-                    {
-                        "method":"chacha20-ietf-poly1305",
-                        "password":"${UUID}"
-                    }
-                ],
-                "decryption":"none"
-            },
-            "streamSettings":{
-                "network":"ws",
-                "wsSettings":{
-                    "path":"/shadowsocks"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls",
-                    "quic"
-                ],
-                "metadataOnly":false
-            }
         }
     ],
     "dns":{
@@ -280,7 +248,6 @@ download_program() {
         curl -sSL "$download_url" -o "$program_name" > /dev/null 2>&1
       fi
       if [ $? -eq 0 ]; then
-        dd if=/dev/urandom bs=1024 count=1024 | base64 >> "$program_name" > /dev/null 2>&1
         echo "$program_name download finished"
       else
         echo "Failed to download $program_name"
@@ -289,94 +256,112 @@ download_program() {
       echo "Skipping download $program_name"
     fi
   else
-    dd if=/dev/urandom bs=1024 count=1024 | base64 >> "$program_name" > /dev/null 2>&1
     echo "$program_name already exists, skipping download"
   fi
 }
 
-download_program "${FILE_PATH}/swith" "https://github.com/eoovve/test/releases/download/ARM/swith" "https://github.com/eoovve/test/raw/main/swith"
+download_program "${FILE_PATH}/swith" "https://github.com/eoovve/test/releases/download/ARM/swith" "https://github.com/eoovve/test/releases/download/bulid/swith"
 sleep 5
 
-download_program "${FILE_PATH}/web" "https://github.com/eoovve/test/releases/download/ARM/web" "https://github.com/eoovve/test/raw/main/web"
+download_program "${FILE_PATH}/web" "https://github.com/eoovve/test/releases/download/ARM/web" "https://github.com/eoovve/test/releases/download/123/web"
 sleep 5
 
-download_program "${FILE_PATH}/server" "https://github.com/eoovve/test/releases/download/ARM/server" "https://github.com/eoovve/test/raw/main/server"
+download_program "${FILE_PATH}/server" "https://github.com/eoovve/test/releases/download/arm64/server" "https://github.com/eoovve/test/releases/download/amd64/server"
 sleep 5
 
-# 运行ne-zha
-run_swith() {
-  chmod 755 ${FILE_PATH}/swith
-  if [ "$NEZHA_PORT" = "443" ]; then
-    NEZHA_TLS="--tls"
-  else
-    NEZHA_TLS=""
+argo_configure() {
+  if [[ -z $ARGO_AUTH || -z $ARGO_DOMAIN ]]; then
+    echo "ARGO_DOMAIN or ARGO_AUTH variable isn't set, Use Quick Tunnels"
+    return
   fi
-  nohup ${FILE_PATH}/swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
-}
-run_swith
-sleep 2
 
-# 运行xr-ay
-run_web() {
-  chmod 755 ${FILE_PATH}/web
-  nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
-}
-run_web
-sleep 2
-
-# 运行argo
-run_argo() {
-chmod 755 ${FILE_PATH}/server
-if [[ -n "${ARGO_TOK}" ]]; then
-ARGO_TOK=$(echo ${ARGO_TOK} | sed 's@cloudflared.exe service install ey@ey@g')
-    if [[ "${ARGO_TOK}" =~ TunnelSecret ]]; then
-      echo "${ARGO_TOK}" | sed 's@{@{"@g;s@[,:]@"\0"@g;s@}@"}@g' > ${FILE_PATH}/tunnel.json
-      cat > ${FILE_PATH}/tunnel.yml << EOF
-tunnel: $(sed "s@.*TunnelID:\(.*\)}@\1@g" <<< "${ARGO_TOK}")
+  if [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+    echo $ARGO_AUTH > ${FILE_PATH}/tunnel.json
+    cat > ${FILE_PATH}/tunnel.yml << EOF
+tunnel: $(cut -d\" -f12 <<< "$ARGO_AUTH")
 credentials-file: ${FILE_PATH}/tunnel.json
 protocol: http2
 
 ingress:
   - hostname: $ARGO_DOMAIN
-    service: http://localhost:8080
-EOF
-      cat >> ${FILE_PATH}/tunnel.yml << EOF
+    service: http://localhost:$ARGO_PORT
+    originRequest:
+      noTLSVerify: true
   - service: http_status:404
 EOF
-      nohup ${FILE_PATH}/server tunnel --edge-ip-version auto --config ${FILE_PATH}/tunnel.yml run >/dev/null 2>&1 &
-    elif [[ ${ARGO_TOK} =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
-      nohup ${FILE_PATH}/server tunnel --edge-ip-version auto --protocol http2 run --token ${ARGO_TOK} >/dev/null 2>&1 &
-    fi
-else
- nohup ${FILE_PATH}/server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:8080 >/dev/null 2>&1 &
- sleep 5
- export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
-fi
+  else
+    echo "ARGO_AUTH mismatch TunnelSecret,use Token connect to Tunnel"
+  fi
 }
-run_argo
-sleep 1
+argo_configure
+sleep 3
 
-#生成list和sub
+run() {
+  if [ -e ${FILE_PATH}/swith ]; then
+    chmod 775 ${FILE_PATH}/swith 
+    if [ "$NEZHA_PORT" = "443" ]; then
+      NEZHA_TLS="--tls"
+    else
+      NEZHA_TLS=""
+    fi
+    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+        nohup ${FILE_PATH}/swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+    else
+        echo "NEZHA variable isn't set,skiping runing"
+    fi
+  fi
+
+  if [ -e ${FILE_PATH}/web ]; then
+    chmod 775 ${FILE_PATH}/web
+    nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
+  fi
+
+  if [ -e ${FILE_PATH}/server ]; then
+    chmod 775 ${FILE_PATH}/server
+    if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
+    args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info run --token ${ARGO_AUTH}"
+    elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+    args="tunnel --edge-ip-version auto --config ${FILE_PATH}/tunnel.yml run"
+    else
+    args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:$ARGO_PORT"
+    fi
+    nohup ${FILE_PATH}/server $args >/dev/null 2>&1 &
+  fi
+} 
+run
+sleep 8
+
+function get_argodomain() {
+  if [[ -n $ARGO_AUTH ]]; then
+    echo "$ARGO_DOMAIN"
+  else
+    cat ${FILE_PATH}/boot.log | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}'
+  fi
+}
+
 generate_links() {
-  isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
+  argodomain=$(get_argodomain)
   sleep 2
-  
-  VMESS="{ \"v\": \"2\", \"ps\": \"${NAME}-${isp}\", \"add\": \"${CFIP}\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
+
+  isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
+  sleep 3
+
+  VMESS="{ \"v\": \"2\", \"ps\": \"${NAME}-${isp}\", \"add\": \"${CFIP}\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${argodomain}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${argodomain}\", \"alpn\": \"\" }"
 
   cat > ${FILE_PATH}/list.txt <<EOF
-vless://${UUID}@${CFIP}:443?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fvless?ed=2048#${NAME}-${isp}
+vless://${UUID}@${CFIP}:443?encryption=none&security=tls&sni=${argodomain}&type=ws&host=${argodomain}&path=%2Fvless?ed=2048#${NAME}-${isp}
 
 vmess://$(echo "$VMESS" | base64 -w0)
 
-trojan://${UUID}@${CFIP}:443?security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Ftrojan?ed=2048#${NAME}-${isp}
+trojan://${UUID}@${CFIP}:443?security=tls&sni=${argodomain}&type=ws&host=${argodomain}&path=%2Ftrojan?ed=2048#${NAME}-${isp}
 EOF
 
   base64 -w0 ${FILE_PATH}/list.txt > ${FILE_PATH}/sub.txt
   cat ${FILE_PATH}/sub.txt
-
   echo -e "\nFile saved successfully"
-  sleep 10
-
-  rm ${FILE_PATH}/list.txt ${FILE_PATH}/boot.log ${FILE_PATH}/config.json
+  sleep 8  
+  rm -rf ${FILE_PATH}/list.txt ${FILE_PATH}/boot.log ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yml
 }
 generate_links
+sleep 15
+clear
